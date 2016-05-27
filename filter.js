b7d17b90fcdc610d72dd5a6cff2388fd8924bcd0
daevoobrak <daevoobrak@gmail.com>
2016-05-27 18:30:31 +0530

    This is Testing

--- filter.js
+++ filter.js
@@ -11,3 +11,8 @@ weatherApp.filter('toDate',function(){
     return Math.floor(val);
   }
 })
+.filter('capitalizeFirst',function(){
+  return function (str) {
+     return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+})
